c34b1ab5f5b38085c619c54e6ac118f28060ff78
Feng Qinxuan <939526908@qq.com>
2022-11-08 10:11:44 +0800

    Delete merge.py

--- merge.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-df1=pd.read_csv('bilibili.csv')
-df2=pd.read_csv('bilibili_detail.csv')
-
-df=pd.merge(df1,df2,on='media_id')
-df.to_csv(r'E:\数据分析方法\18124399冯秦萱\bilibili_data.csv',index=False,encoding="utf_8_sig")
-print("文件输出完毕")
